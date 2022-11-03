da90935d613fab761055c50a865245e760909815
KonosukeTakano <bado.bado.0408@gmail.com>
2022-11-03 11:20:19 +0900

    [Add] guestuser_log_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
   resource :users, :only => [:edit, :update]
   devise_for :users
   root to: 'homes#top'
