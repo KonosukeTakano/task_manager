f8807b48b6980b3a68de493bf89db29f8d7ff9c1
KonosukeTakano <bado.bado.0408@gmail.com>
2022-11-08 19:37:30 +0900

    [Upd] production.rb, config mailer

--- lib/batch/remind_task.rb
+++ lib/batch/remind_task.rb
@@ -3,7 +3,7 @@ class Batch::RemindTask
     tasks = Task.all
     tasks.each do |task|
       time_difference = (task.due - Time.now.in_time_zone("Tokyo")) / 3600 # AM9:00との時間差を時間単位で算出する
-      if time_difference <= 24
+      if time_difference <= 24 && time_difference >= 0
         SampleMailer.send_before_deadline(task.user).deliver_now # メールを送信するためのメソッド
         p "メールを#{task.user.name}に送信しました" # ログに表示するメッセージ
       end
